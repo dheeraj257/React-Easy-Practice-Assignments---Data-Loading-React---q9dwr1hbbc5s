90a5c106f69611ea960ad2caba7a9e5aa0e5af20
dheeraj257 <55332301+dheeraj257@users.noreply.github.com>
2022-04-28 08:27:24 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -10,11 +10,14 @@ const getData = async () =>{
 const Loader = () => <div id="loader">Loading...</div>
 const App = () => {
   const [todos,setTodos] = useState([])
+  const [loading,setloading] = useState(true)
+  
 
   useEffect(()=>{
     getData().then(data=> {
       setTodos(data)
      })
+    setloading(false)
   },[])
   return (
     <div id="main">
